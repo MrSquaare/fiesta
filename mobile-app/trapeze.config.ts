7fd08079cb7b077913e29fc5c7c4f59a9cb2cfcf
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2022-09-04 16:52:37 +0200

    chore: sync app name and id

--- mobile-app/trapeze.config.ts
+++ mobile-app/trapeze.config.ts
@@ -1,5 +1,7 @@
 import { MobileProject, MobileProjectConfig } from "@trapezedev/project";
 
+import capacitorConfig from "./capacitor.config";
+
 const packageJson = require("./package.json");
 
 const config: MobileProjectConfig = {
@@ -18,8 +20,31 @@ const main = async () => {
   const project = new MobileProject("./", config);
   await project.load();
 
+  await project.android?.setPackageName(capacitorConfig.appId!);
   await project.android?.setVersionName(packageJson.version);
+  const xmlFile = project.android?.getResourceXmlFile("values/strings.xml");
+  await xmlFile?.load();
+  xmlFile?.replaceFragment(
+    'resources/string[@name="app_name"]',
+    `<string name="app_name">${capacitorConfig.appName!}</string>`
+  );
+  xmlFile?.replaceFragment(
+    'resources/string[@name="title_activity_main"]',
+    `<string name="title_activity_main">${capacitorConfig.appName!}</string>`
+  );
+  xmlFile?.replaceFragment(
+    'resources/string[@name="package_name"]',
+    `<string name="package_name">${capacitorConfig.appId!}</string>`
+  );
+  xmlFile?.replaceFragment(
+    'resources/string[@name="custom_url_scheme"]',
+    `<string name="custom_url_scheme">${capacitorConfig.appId!}</string>`
+  );
+  await project.ios?.setDisplayName("App", "Debug", capacitorConfig.appName!);
+  await project.ios?.setBundleId("App", "Debug", capacitorConfig.appId!);
   await project.ios?.setVersion("App", "Debug", packageJson.version);
+  await project.ios?.setDisplayName("App", "Release", capacitorConfig.appName!);
+  await project.ios?.setBundleId("App", "Release", capacitorConfig.appId!);
   await project.ios?.setVersion("App", "Release", packageJson.version);
 
   await project.commit();

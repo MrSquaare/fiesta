4be252a7af0a560af703e8e8dbf3b771e38dc6e1
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2022-09-04 12:13:01 +0200

    feat: add reload button

--- mobile-app/src/Root.tsx
+++ mobile-app/src/Root.tsx
@@ -3,19 +3,26 @@ import { Alert, Button, Spinner } from "flowbite-react";
 import { FC } from "react";
 
 export const Root: FC = () => {
-  const { data, loading, error } = useHelloWorldQuery();
+  const { data, loading, error, refetch } = useHelloWorldQuery();
 
   return (
     <div className={"flex h-full flex-col items-center justify-center"}>
       <h1 className={"mb-4 text-5xl"}>Fiesta</h1>
-      {loading ? <Spinner /> : null}
-      {error ? <Alert color={"failure"}>{error.message}</Alert> : null}
-      {data ? (
-        <div className={"flex flex-col items-center justify-center"}>
-          <div>{data.helloWorld}</div>
-          <Button>Let&apos;s party!</Button>
-        </div>
-      ) : null}
+      <div className={"flex flex-col items-center justify-center"}>
+        {loading ? <Spinner /> : null}
+        {error ? (
+          <>
+            <Alert color={"failure"}>{error.message}</Alert>
+            <Button onClick={() => refetch()}>Reload</Button>
+          </>
+        ) : null}
+        {data ? (
+          <>
+            <div>{data.helloWorld}</div>
+            <Button>Let&apos;s party!</Button>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 };
